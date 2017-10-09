d61ef1b6caebfcd991edafa05806b90de62b0497
Joseph Zennamo <jzennamo@uchicago.edu>
2017-10-09 15:44:17 -0500

    Added a readme for a simple C-macro

---
+++ setup_gallery.sh
@@ -0,0 +1,4 @@
+source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setup
+source /cvmfs/sbnd.opensciencegrid.org/products/sbnd/setup_sbnd.sh
+source /cvmfs/fermilab.opensciencegrid.org/products/larsoft/setup
+setup larsoftobj v1_13_00 -q e10:prof
